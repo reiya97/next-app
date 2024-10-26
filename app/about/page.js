507f756dde4d1f8407552b60a189d91faa69984f
reiya97 <77827863+reiya97@users.noreply.github.com>
2024-10-26 19:45:37 +0900

    add: add about page

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+import { Footer } from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>About page</h1>
+        <div className={styles.ctas}></div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
