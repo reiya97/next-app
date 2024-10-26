98e8096608f5f1c6f3ae1bb792d9991d1f883125
reiya97 <77827863+reiya97@users.noreply.github.com>
2024-10-26 20:47:54 +0900

    fix: change <a> to <Link> tag

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,11 +1,13 @@
 import styles from "../page.module.css";
+import Link from 'next/link';
 
 export function Header() {
   return (
     <div>
         <ul className="header-list">
-            <li><a href="/">Index</a></li>
-            <li><a href="/about">About</a></li>
+            
+            <li><Link href="/">Index</Link></li>
+            <li><Link href="/about">About</Link></li>
         </ul>
     </div>
   );
