bbf4e8c1b203bcddfa42f6c033b899ea1d3b9c85
reiya97 <77827863+reiya97@users.noreply.github.com>
2024-10-26 19:44:48 +0900

    add : componentize footer section

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from "../page.module.css";
+
+export function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>
+        Here is common footer section written in components/Footer.jsx
+      </footer>
+
+    </>
+      
+  );
+}
