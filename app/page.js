045249b1caeae0fc9ee6d7b76bc376ce451c31d3
reiya97 <77827863+reiya97@users.noreply.github.com>
2024-10-27 14:26:47 +0900

    add: add some props for learning

--- app/page.js
+++ app/page.js
@@ -1,14 +1,24 @@
+"use client";
 import styles from "./page.module.css";
 import { Footer } from "./components/Footer";
-import { Headline } from "./components/Headline";
+import { Main } from "./components/Main";
 import { Header } from "./components/Header";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <Header />
-      <Headline title="Index Page" page="page.js"/>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <Header />
+            <Main
+                title="Index Page"
+                page="page.js" //string
+                num={100} //number
+                array={[1,2,3]} //array
+                obj={{foo: "foo", bar: "bar"}} //object
+                bool //boolean
+                comp={<div>component of props</div>} //component
+                onClick={() => alert("cliecked")} //function
+            />
+            <Footer />
+        </div>
+    );
 }
