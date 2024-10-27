179f826506878994f5ddb595098cefdff16e2eae
reiya97 <77827863+reiya97@users.noreply.github.com>
2024-10-27 15:35:53 +0900

    add : contact page

---
+++ app/contact/page.js
@@ -0,0 +1,14 @@
+import styles from "../page.module.css";
+import { Footer } from "../components/Footer";
+import { Main } from "../components/Main";
+import { Header } from "../components/Header";
+
+export default function Home() {
+    return (
+        <div className={styles.page}>
+            <Header />
+            <h1>Contact Page</h1>
+            <Footer />
+        </div>
+    );
+}
