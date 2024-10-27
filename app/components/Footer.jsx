a410ac82884ea062accaf6496536167595a37222
reiya97 <77827863+reiya97@users.noreply.github.com>
2024-10-27 14:25:11 +0900

    clean: refactoring

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,12 +1,11 @@
 import styles from "../page.module.css";
 
 export function Footer(props) {
-  return (
-    <div>
-      <footer className={styles.footer}>
-        <p>Here is common footer section defined in components/{props.page}.jsx</p>
-      </footer>
-    </div>
-      
-  );
+    return (
+        <div>
+            <footer className={styles.footer}>
+                <p>Here is common footer section defined in components/{props.page}.jsx</p>
+            </footer>
+        </div>
+    );
 }
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,13 +2,12 @@ import styles from "../page.module.css";
 import Link from 'next/link';
 
 export function Header() {
-  return (
-    <div>
-        <ul className="header-list">
-            
-            <li><Link href="/">Index</Link></li>
-            <li><Link href="/about">About</Link></li>
-        </ul>
-    </div>
-  );
+    return (
+        <div>
+            <ul className="header-list">
+                <li><Link href="/">Index</Link></li>
+                <li><Link href="/about">About</Link></li>
+            </ul>
+        </div>
+    );
 }
