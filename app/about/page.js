a7944e369b8e3ac3b9ac07a90160b2aef8887084
reiya97 <77827863+reiya97@users.noreply.github.com>
2024-10-27 14:27:12 +0900

    change: change Headline to Main

--- app/about/page.js
+++ app/about/page.js
@@ -1,14 +1,14 @@
 import styles from "../page.module.css";
 import { Footer } from "../components/Footer";
-import { Headline } from "../components/Headline";
+import { Main } from "../components/Main";
 import { Header } from "../components/Header";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <Header />
-      <Headline title="About Page" page="about/page.js"/>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <Header />
+            <Main title="About Page" page="about/page.js"/>
+            <Footer />
+        </div>
+    );
 }
